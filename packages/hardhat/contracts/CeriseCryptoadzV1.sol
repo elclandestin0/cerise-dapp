f76b0d95c2ddcd02aabe49e57ef049bed5bfaa81
Memo <majd.khoury93@gmail.com>
2021-11-05 23:16:08 -0400

    update contract

--- packages/hardhat/contracts/CeriseCryptoadzV1.sol
+++ packages/hardhat/contracts/CeriseCryptoadzV1.sol
@@ -3,13 +3,15 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "hardhat/console.sol";
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-contract CeriseCryptoadzV1 {
-    // event SetPurpose(address sender, string purpose);
+contract CeriseCryptoadzV1 is Ownable, IERC721 {
     address public gremplin = 0x4298e663517593284ad4fe199b21815bd48a9969;
-    address public infernaltoast = 0x7132c9f36abe62eab74cdfdd08c154c9ae45691b;
+    string public gremplinTokenURI = "";
+    address public infernalToast = 0x7132c9f36abe62eab74cdfdd08c154c9ae45691b;
+    string public infernalToastURI = "";
+
     string public name = "Cerise Cryptoadz V1";
 
     // modifiers
@@ -18,9 +20,21 @@ contract CeriseCryptoadzV1 {
         _;
     }
 
-    modifier onlyInfernal() {
-        require(msg.sender == infernal, "Only Infernal can mint!");
+    modifier onlyInfernalToast() {
+        require(msg.sender == infernal, "Only InfernalToast can mint!");
         _;
+    }
+
+    // Mints only for Gremplin
+    function gremplinMint() external onlyGremplin {
+        _mint(gremplin, 21);
+        _setTokenURI(21, gremplinTokenURI);
+    }
+
+    // Mints only for Infernal toast
+    function infernalToastMint() external onlyInfernalToast {
+        _mint(infernalToast, 22);
+        _setTokenURI(22, infernalToastURI);
     }
 
     constructor() {
