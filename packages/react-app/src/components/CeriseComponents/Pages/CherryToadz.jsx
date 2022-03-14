6ddf83ec937487bfd3d78ede257ba5c942fdea93
memo khoury <majd.khoury93@gmail.com>
2022-03-13 21:22:20 -0400

    updates

--- packages/react-app/src/components/CeriseComponents/Pages/CherryToadz.jsx
+++ packages/react-app/src/components/CeriseComponents/Pages/CherryToadz.jsx
@@ -8,7 +8,8 @@ import { Transactor } from "../../../helpers";
 
 // assets
 import Toadz from "../../../assets/logo";
-import DigFashion from "../../../assets/dig-fashion-sample.gif";
+import left from "../../../assets/toadz_leftarrow.svg";
+import right from "../../../assets/toadz_rightarrow.svg";
 
 // cerise components
 import MintButton from "../Buttons/MintButton";
@@ -45,6 +46,8 @@ export default function CherryToadz({
 }) {
   const ipfsFolder = `ipfs.io/ipfs/QmYS3HzeGNr5jVbSsRfv9pH7DeinEsvtiTtPuTzD7DBAKR/1`;
   const tx = Transactor(signer, gasPrice);
+  
+  
   // States to show different mint types on the button
   const [claimable, setClaimable] = useState();
   const [nullAddress, setNullAddress] = useState(false);
@@ -58,9 +61,9 @@ export default function CherryToadz({
   const [ifOwner, setIfOwner] = useState(false);
   const [ifBurnt, setIfBurnt] = useState("");
 
-  // states to set selected metadata
-  const [token, setToken] = useState({});
-  const [tokenId, setTokenId] = useState(0);
+
+  // fix states
+  const [tokenId, setTokenId] = useState(1);
 
   useEffect(() => {
     console.log(tokenId);
@@ -70,41 +73,35 @@ export default function CherryToadz({
       return;
     }
 
-    // set initial tokenId to 0
-    setToken(metadata[tokenId]);
-
     if (address == "0x7132c9f36abe62eab74cdfdd08c154c9ae45691b") setIsInfernal(true);
     if (address == "0xc5f59709974262c4afacc5386287820bdbc7eb3a") setIsFarokh(true);
     if (address == "0x4298e663517593284ad4fe199b21815bd48a9969") setIsGremplin(true);
     if (address == "0x8bd8795cbeed15f8d5074f493c53b39c11ed37b2") setIsMoti(true);
     if (address == "0xe0110C6EE2138Ecf9962a6f9f6Ad329cDFE1FA17") setIsCerise(true);
 
-    console.log("address", address);
     const proof = merkleTree.getHexProof(hashOwner(address));
     const leaf = hashOwner(address);
     const root = merkleTree.getHexRoot();
     setNullAddress(false);
     setClaimable(merkleTree.verify(proof, leaf, root));
-  }, [address, didMint, ifBurnt, ifOwner, tokenId]);
+  }, [address, didMint, ifBurnt, ifOwner]);
 
-  const moveTokenId = forward => {
+  const moveTokenId = (forward) => {
     if (forward) {
-      let increment = tokenId + 1;
-      console.log(increment);
-      if (increment >= metadata.length) {
-        increment = 0;
-        setTokenId(increment);
+      if (tokenId == 5) {
+        setTokenId(1);
       } else {
-        setTokenId(increment);
+        const id = tokenId + 1;
+        console.log(id);
+        setTokenId(id);
       }
     } else {
-      let decrement = tokenId - 1;
-      console.log(decrement);
-      if (decrement < 0) {
-        decrement = metadata.length - 1;
-        setTokenId(decrement);
+      if (tokenId == 1) {
+        setTokenId(5);
       } else {
-        setTokenId(decrement);
+        const id = tokenId - 1;
+        console.log(id);
+        setTokenId(id);
       }
     }
   };
@@ -130,9 +127,6 @@ export default function CherryToadz({
     );
   };
 
-  const burnToken = async tokenId => {
-    await tx(writeContracts.CherryToadz.burn(tokenId));
-  };
 
   const contracts = useContractLoader(provider, contractConfig, chainId);
   let contract;
@@ -144,30 +138,13 @@ export default function CherryToadz({
   return (
     <div>
       <div className="bg-test bg-cover bg-no-repeat bg-center text-primary image-height">
-        <div className="pt-56 flex items-center justify-center text-center">
+        <div className="pt-72 flex items-center justify-center text-center">
           <div>
             <img class="" src={Toadz} />
           </div>
         </div>
-        <div className="pt-40 flex items-center justify-center text-center">
-          <div>
-            <MintButton popCherry={popCherry}>Mint</MintButton>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div className="flex justify-center">
-          <h1 className="font-h1 text-neonGreen text-4xl px-5 pt-16 text-center text-neonRed">
-            <span>22</span> CrypToadz Street Wearables
-          </h1>
-        </div>
-
         {claimable && !didMint && (
           <div>
-            <div className="flex justify-center">
-              <MintButton popCherry={popCherry}>Mint</MintButton>
-            </div>
             <div>
               <p class="text-center text-2xl font-h1 p-4 text-neonGreen">
                 {isGremplin
@@ -176,9 +153,8 @@ export default function CherryToadz({
                   ? "Thanks for sending me down the NFT rabbit hole!"
                   : claimable && isFarokh
                   ? "Thanks for sharing about TOADZ on Twitter!"
-                  : claimable && isMoti
-                  ? "Thanks for creating the best community ever!"
-                  : "!vibe"}
+                  : "Thanks for creating the best community ever!"}
+                  
               </p>
             </div>
           </div>
@@ -200,16 +176,29 @@ export default function CherryToadz({
             <p class="text-center text-neonGreen text-2xl font-h1 p-4 px-5 pt-16">Login to check if you own a toad!</p>
           </div>
         )}
-        <div className="flex justify-center">
+        {/* <div className="flex justify-center">
           <p className="text-neonGreen text-base font-h1 text-neonGreen text-justify px-3 md:px-24 lg:px-48 xl:px-96">
             There are 22 exclusive street wearables items available to mint for any{" "}
             <a href="https://cryptoadz.io/">CrypTOADZ</a> owners. These include cargo pants, jackets, hoodies and tees.
             Burning your token will allow you to enter your shipping address and get your tokenized street wearable!
           </p>
+        </div> */}
+        <div className="pt-30 flex items-center justify-center text-center">
+          <div>
+            <MintButton popCherry={popCherry}>Mint</MintButton>
+          </div>
         </div>
       </div>
-      <div className="flex justify-center pb-5 pt-5 px-10">
-        <Card className="bg-footer">
+
+      <div>
+        <div className="flex justify-center">
+          <h1 className="font-h1 text-neonGreen text-4xl px-5 pt-16 text-center text-neonRed">
+            <span>22</span> CrypToadz Street Wearables
+          </h1>
+        </div>
+
+        <div className="flex justify-center pb-5 pt-5 px-10">
+        <Card>
           {/* <CardImage
                 src={DigFashion}
                 alt="Card Image"
@@ -221,20 +210,22 @@ export default function CherryToadz({
                   moveTokenId(false);
                 }}
               >
-                test
+              <img style={{width: "50px"}} src={left}/>
               </Button>
-              <ReactPlayer url={token.image} />
+              <img src={`https://ipfs.io/ipfs/QmZs8Kn36rEngwHbn6M5WdP6cUvhHP4g2kTj7jFq44vjPs/${tokenId}.gif`}/>
               <Button
                 onClick={() => {
                   moveTokenId(true);
                 }}
               >
-                test{" "}
+                <img style={{width: "50px"}} src={right}/>
               </Button>
             </div>
           </CardBody>
         </Card>
       </div>
+      </div>
+
     </div>
   );
 }
