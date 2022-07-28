9c962737ceff22630b32ac62c6fa653e5e758052
memo khoury <majd.khoury93@gmail.com>
2022-07-28 01:01:22 -0400

    get balance

--- packages/react-app/src/components/CeriseComponents/Pages/CherryToadz.jsx
+++ packages/react-app/src/components/CeriseComponents/Pages/CherryToadz.jsx
@@ -14,11 +14,8 @@ import MintButton from "../Buttons/MintButton";
 import Card from "@material-tailwind/react/Card";
 import CardBody from "@material-tailwind/react/CardBody";
 
-// merkle tree stuff
-import { MerkleTree } from "merkletreejs";
-const keccak256 = require("keccak256");
-const tree = require("../../../utils/merkle-tree.json");
 import { ethers } from "ethers";
+import { useMemo } from "react";
 
 export default function CherryToadz({
   customContract,
@@ -32,44 +29,29 @@ export default function CherryToadz({
   writeContracts,
 }) {
   const tx = Transactor(signer, gasPrice);
-
   const [isInfernal, setIsInfernal] = useState(false);
   const [isGremplin, setIsGremplin] = useState(false);
   const [isFarokh, setIsFarokh] = useState(false);
   const [isMoti, setIsMoti] = useState(false);
   const [isCerise, setIsCerise] = useState(false);
   const [isCozomo, setIsCozomo] = useState(false);
-  const [didMint, setDidMint] = useState(false);
-  const [isPublicSale, setIsPublicSale] = useState(false);
   // fix states
 
   const contracts = useContractLoader(provider, contractConfig, chainId);
-  useEffect(() => {
-    if (!address && !contracts) {
-      return;
-    }
 
-    if (address == "0x7132c9f36abe62eab74cdfdd08c154c9ae45691b") setIsInfernal(true);
-    if (address == "0xc5f59709974262c4afacc5386287820bdbc7eb3a") setIsFarokh(true);
-    if (address == "0x4298e663517593284ad4fe199b21815bd48a9969") setIsGremplin(true);
-    if (address == "0xCe90a7949bb78892F159F428D0dC23a8E3584d75") setIsCozomo(true);
-    if (address == "0x8bd8795cbeed15f8d5074f493c53b39c11ed37b2") setIsMoti(true);
-    if (address == "0xe0110C6EE2138Ecf9962a6f9f6Ad329cDFE1FA17") setIsCerise(true);
+  const isPublicSale = useMemo(async () => {
+    return await contracts?.["CherryToadz"].isPublicSale();
+  }, [contracts]);
 
-    const getDidMint = async () => {
-      const memo = await contracts?.["CherryToadz"].didMint(address);
-      setDidMint(memo);
-    };
+  const didMint = useMemo(async () => {
+    return await contracts?.["CherryToadz"].didMint(address);
+  }, [contracts]);
 
-    const getIsPublicSale = async () => {
-      const sale_ = await contracts?.["CherryToadz"].isPublicSale();
-      setIsPublicSale(sale_);
-    };
-
-    if (address && contracts) {
-      getDidMint();
-    }
-  }, [address, didMint, contracts]);
+  const getBalance = useMemo(async () => {
+    await contracts?.["CherryToadz"].balanceOf(address).then(x => {
+      return x;
+    });
+  }, [contracts, address]);
 
   const popCherry = async () => {
     await tx(
@@ -79,6 +61,18 @@ export default function CherryToadz({
       }),
     );
   };
+
+  useEffect(() => {
+    if (!address && !contracts) {
+      return;
+    }
+    if (address === "0x7132c9f36abe62eab74cdfdd08c154c9ae45691b") setIsInfernal(true);
+    if (address === "0xc5f59709974262c4afacc5386287820bdbc7eb3a") setIsFarokh(true);
+    if (address === "0x4298e663517593284ad4fe199b21815bd48a9969") setIsGremplin(true);
+    if (address === "0xCe90a7949bb78892F159F428D0dC23a8E3584d75") setIsCozomo(true);
+    if (address === "0x8bd8795cbeed15f8d5074f493c53b39c11ed37b2") setIsMoti(true);
+    if (address === "0xe0110C6EE2138Ecf9962a6f9f6Ad329cDFE1FA17") setIsCerise(true);
+  }, [address, contracts]);
 
   let contract;
   if (!customContract) {
@@ -111,9 +105,9 @@ export default function CherryToadz({
                   : isCerise && !didMint
                   ? "I'd like to thank me for being me!"
                   : isPublicSale && !didMint
-                  ? "Croak!"
+                  ? `Croak! }`
                   : isPublicSale && didMint
-                  ? "Croak Again"
+                  ? `Croak! You have ${getBalance.toString()} clothes you can redeem!`
                   : "The Uncroakening"}
               </p>
             </div>
