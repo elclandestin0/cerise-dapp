08bc81cc91e3ca1feaf77607af66763f13bd0f4d
cherrypopsicle <majd.khoury93@gmail.com>
2022-11-30 09:22:30 +0400

    stuff

--- packages/react-app/tailwind.config.js
+++ packages/react-app/tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       red: "#ff0000",
     }),
     backgroundImage: {
-      test: "url('https://www.dropbox.com/s/fb3vhqzehmpm1t6/promo.gif?raw=1')",
+      test: "url('https://previews.dropbox.com/p/thumb/ABsGUBEeSafoEM44WDqbQ5w8Esh6kUGcCCtNDZkHr7104Dhahw5t4bEV0UBlS3sfTBQApCkZghEZW845M4Gzui8lztKkTqnudenAPswAKQdADqsuE9whOAIG6YA_tDH91om0IgSpfK18RppZ8He5qOVdOoLT_RD-9n80xq57HZZ4WcyLEYsmA7BaUZvSi55IinVTSx2X9IrDcgOPmFE-S2gncK7UeMsHrpdTgrm2BSz5wDwXmgqaiFIZhH2s_uhfLnh8T03zEXTgGAnihfq462Z0GMy4MqKubgLWrE5Qj9nLq3hdTx6sR10RswC9yXy4moTA-kCdyH307GljnUC1424M9I6Q-ZZSTLr8xeWVfs_jqQtQ8ovvFey-zrbxGKDfCgeFvxKLsAmN6jvQ_0a3egfl/p.gif')",
       burn: "url('https://c.tenor.com/WhD4AWN30YkAAAAC/clouds-moving.gif')",
     },
     textColor: {
